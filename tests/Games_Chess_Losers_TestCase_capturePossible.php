c3aace5f650d288aa5fc6c14ee81fc03bede7bfb
Greg Beaver <cellog@php.net>
2004-02-15 06:10:41 +0000

    start on test case
    git-svn-id: http://svn.php.net/repository/pear/packages/Games_Chess/trunk@151414 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/Games_Chess_Losers_TestCase_capturePossible.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * API Unit tests for Games_Chess package.
+ * 
+ * @version    $Id$
+ * @author     Laurent Laville <pear@laurent-laville.org> portions from HTML_CSS
+ * @author     Greg Beaver
+ * @package    Games_Chess
+ */
+
+/**
+ * @package Games_Chess
+ */
+
+class Games_Chess_Losers_TestCase_capturePossible extends PHPUnit_TestCase
+{
+    /**
+     * A Games_Chess_Standard object
+     * @var        object
+     */
+    var $board;
+
+    function Games_Chess_Losers_TestCase_capturePossible($name)
+    {
+        $this->PHPUnit_TestCase($name);
+    }
+
+    function setUp()
+    {
+        error_reporting(E_ALL);
+        $this->errorOccured = false;
+        set_error_handler(array(&$this, 'errorHandler'));
+
+        $this->board = new Games_Chess_Losers();
+        $this->board->blankBoard();
+    }
+
+    function tearDown()
+    {
+        unset($this->board);
+    }
+
+    function _stripWhitespace($str)
+    {
+        return preg_replace('/\\s+/', '', $str);
+    }
+
+    function _methodExists($name) 
+    {
+        if (in_array(strtolower($name), get_class_methods($this->board))) {
+            return true;
+        }
+        $this->assertTrue(false, 'method '. $name . ' not implemented in ' . get_class($this->board));
+        return false;
+    }
+
+    function errorHandler($errno, $errstr, $errfile, $errline) {
+        //die("$errstr in $errfile at line $errline: $errstr");
+        $this->errorOccured = true;
+        $this->assertTrue(false, "$errstr at line $errline, $errfile");
+    }
+    
+    function test_invalid_1()
+    {
+        if (!$this->_methodExists('_capturePossible')) {
+            return;
+        }
+    }
+}
+
+?>
