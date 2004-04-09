06047c60a9239c74cf924431f7a83ad55d52c0d0
Greg Beaver <cellog@php.net>
2004-04-09 06:32:16 +0000

    finish test case
    git-svn-id: http://svn.php.net/repository/pear/packages/Games_Chess/trunk@155625 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Games_Chess_Losers_TestCase_gameOver.php
+++ tests/Games_Chess_Losers_TestCase_gameOver.php
@@ -85,6 +85,171 @@ class Games_Chess_Losers_TestCase_gameOver extends PHPUnit_TestCase
         $this->board->addPiece('W', 'K', 'f3');
         $this->board->_move = 'B';
         $this->assertEquals('W', $this->board->gameOver());
+        $this->board->_move = 'W';
+        $this->assertEquals('W', $this->board->gameOver());
+    }
+
+    function test_valid_black_nopieces()
+    {
+        if (!$this->_methodExists('_validMove')) {
+            return;
+        }
+        if (!$this->_methodExists('_parseMove')) {
+            return;
+        }
+        if (!$this->_methodExists('addPiece')) {
+            return;
+        }
+        if (!$this->_methodExists('blankBoard')) {
+            return;
+        }
+        if (!$this->_methodExists('resetGame')) {
+            return;
+        }
+        $this->board->resetGame();
+        $this->board->blankBoard();
+        $this->board->addPiece('W', 'K', 'e8');
+        $this->board->addPiece('W', 'R', 'a8');
+        $this->board->addPiece('B', 'K', 'f3');
+        $this->board->_move = 'B';
+        $this->assertEquals('B', $this->board->gameOver());
+        $this->board->_move = 'W';
+        $this->assertEquals('B', $this->board->gameOver());
+    }
+
+    function test_invalid_piecesleft()
+    {
+        if (!$this->_methodExists('_validMove')) {
+            return;
+        }
+        if (!$this->_methodExists('_parseMove')) {
+            return;
+        }
+        if (!$this->_methodExists('addPiece')) {
+            return;
+        }
+        if (!$this->_methodExists('blankBoard')) {
+            return;
+        }
+        if (!$this->_methodExists('resetGame')) {
+            return;
+        }
+        $this->board->resetGame();
+        $this->board->blankBoard();
+        $this->board->addPiece('W', 'K', 'e8');
+        $this->board->addPiece('W', 'R', 'a8');
+        $this->board->addPiece('B', 'K', 'f3');
+        $this->board->addPiece('B', 'R', 'a3');
+        $this->board->_move = 'B';
+        $this->assertFalse($this->board->gameOver(), '1');
+        $this->board->_move = 'W';
+        $this->assertFalse($this->board->gameOver(), '1');
+    }
+
+    function test_valid_white_checkmated()
+    {
+        if (!$this->_methodExists('_validMove')) {
+            return;
+        }
+        if (!$this->_methodExists('_parseMove')) {
+            return;
+        }
+        if (!$this->_methodExists('addPiece')) {
+            return;
+        }
+        if (!$this->_methodExists('blankBoard')) {
+            return;
+        }
+        if (!$this->_methodExists('resetGame')) {
+            return;
+        }
+        $this->board->resetGame();
+        $this->board->blankBoard();
+        $this->board->addPiece('W', 'K', 'h8');
+        $this->board->addPiece('B', 'K', 'g7');
+        $this->board->addPiece('B', 'R', 'h3');
+        $this->assertEquals('W', $this->board->gameOver());
+    }
+
+    function test_valid_black_checkmated()
+    {
+        if (!$this->_methodExists('_validMove')) {
+            return;
+        }
+        if (!$this->_methodExists('_parseMove')) {
+            return;
+        }
+        if (!$this->_methodExists('addPiece')) {
+            return;
+        }
+        if (!$this->_methodExists('blankBoard')) {
+            return;
+        }
+        if (!$this->_methodExists('resetGame')) {
+            return;
+        }
+        $this->board->resetGame();
+        $this->board->blankBoard();
+        $this->board->addPiece('B', 'K', 'h8');
+        $this->board->addPiece('W', 'K', 'g7');
+        $this->board->addPiece('W', 'R', 'h3');
+        $this->board->_move = 'B';
+        $this->assertEquals('B', $this->board->gameOver());
+    }
+
+    function test_valid_white_draw()
+    {
+        if (!$this->_methodExists('_validMove')) {
+            return;
+        }
+        if (!$this->_methodExists('_parseMove')) {
+            return;
+        }
+        if (!$this->_methodExists('addPiece')) {
+            return;
+        }
+        if (!$this->_methodExists('blankBoard')) {
+            return;
+        }
+        if (!$this->_methodExists('resetGame')) {
+            return;
+        }
+        $this->board->resetGame();
+        $this->board->blankBoard();
+        $this->board->addPiece('W', 'K', 'h1');
+        $this->board->addPiece('W', 'P', 'a3');
+        $this->board->addPiece('B', 'P', 'a4');
+        $this->board->addPiece('B', 'P', 'h2');
+        $this->board->addPiece('B', 'K', 'h3');
+        $this->assertEquals('D', $this->board->gameOver());
+    }
+
+    function test_valid_black_draw()
+    {
+        if (!$this->_methodExists('_validMove')) {
+            return;
+        }
+        if (!$this->_methodExists('_parseMove')) {
+            return;
+        }
+        if (!$this->_methodExists('addPiece')) {
+            return;
+        }
+        if (!$this->_methodExists('blankBoard')) {
+            return;
+        }
+        if (!$this->_methodExists('resetGame')) {
+            return;
+        }
+        $this->board->resetGame();
+        $this->board->blankBoard();
+        $this->board->addPiece('B', 'K', 'h8');
+        $this->board->addPiece('W', 'P', 'a3');
+        $this->board->addPiece('B', 'P', 'a4');
+        $this->board->addPiece('W', 'P', 'h7');
+        $this->board->addPiece('W', 'K', 'h6');
+        $this->board->_move = 'B';
+        $this->assertEquals('D', $this->board->gameOver());
     }
 }
 
