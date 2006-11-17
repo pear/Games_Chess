c2644fe564c08aeb5a3cd014b4a4b9e43cdc70e8
Greg Beaver <cellog@php.net>
2006-11-17 05:44:54 +0000

    prepare for RC1
    unit testing is complete
    
    
    git-svn-id: http://svn.php.net/repository/pear/packages/Games_Chess/trunk@223519 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ package.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This is the package.xml generator for PEAR_PackageFileManager2
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   PEAR
+ * @package    PEAR_PackageFileManager
+ * @author     Greg Beaver <cellog@php.net>
+ * @copyright  2005-2006 The PHP Group
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/PEAR_PackageFileManager
+ * @since      File available since Release 1.6.0
+ */
+require_once 'PEAR/PackageFileManager2.php';
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+$packagexml = &PEAR_PackageFileManager2::importOptions(dirname(__FILE__) . DIRECTORY_SEPARATOR .
+    'package.xml', array(
+      'exceptions' => array(
+          'LICENSE' => 'doc'),
+      'filelistgenerator' => 'cvs',
+      'packagedirectory' => dirname(__FILE__),
+      'changelogoldtonew' => false,
+      'baseinstalldir' => 'Games',
+      'simpleoutput' => true));
+$packagexml->setNotes('fix bugs in Crazyhouse discovered in unit testing');
+$packagexml->addIgnore(array('package.php','*.tgz','package.xml'));
+$packagexml->setPackageType('php');
+$packagexml->addRelease();
+$packagexml->clearDeps();
+$packagexml->setChannel('pear.php.net');
+$packagexml->setLicense('PHP License 3.01', 'http://www.php.net/license/3_01.txt');
+$packagexml->setReleaseVersion('1.0.0RC1');
+$packagexml->setAPIVersion('1.0.0');
+$packagexml->setReleaseStability('beta');
+$packagexml->setAPIStability('stable');
+$packagexml->setPhpDep('4.2.0');
+$packagexml->setPearinstallerDep('1.4.3');
+$packagexml->generateContents();
+if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
+    $packagexml->writePackageFile();
+} else {
+    $packagexml->debugPackageFile();
+}
+?>
